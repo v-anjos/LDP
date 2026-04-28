0febe8d3d65ace41bbdb229cfe4606bcf3ca8f46
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2026-04-28 20:10:31 -0300

    Refactor nested loop to use variable 'w' for stars
    Added a new variable 'w' for inner loop in the pattern.

--- escada contrario.c
+++ escada contrario.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int i, j, n;
+    int i, j, w, n;
     scanf("%d", &n);
 
     for (i = 1; i <= n; i++) {
         for (j = 1; j <= n - i; j++) {
             printf(" ");
         }
-        for (j = 1; j <= i; j++) {
+        for (w = 1; w <= i; w++) {
             printf("*");
         }
         printf("\n");
