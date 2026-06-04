075eedb996e021ca1f7e4da4d5c9157c91cf22a4
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2026-06-03 21:55:48 -0300

    Update and rename Desafio: Funções, Procedimentos, passagem de parâmetros e Ponteiros (6 pontos).c to Desafio: Funções, Procedimentos, passagem de parâmetros e Ponteiros (6 pontos)1.1.c

--- exemplos/Desafio: Funções, Procedimentos, passagem de parâmetros e Ponteiros (6 pontos)1.1.c
+++ exemplos/Desafio: Funções, Procedimentos, passagem de parâmetros e Ponteiros (6 pontos)1.1.c
@@ -4,14 +4,19 @@ void boas_vindas() {
 	printf("-----Boas Vindas-----");
 }
 
+double arredondar_2_casas(double valor) {
+    return ((int)(valor * 100.0 + 0.5)) / 100.0;
+}
+
 double realizar_venda(double *saldo, double valor_item, double valor_pago){
-	*saldo += valor_item;
+	*saldo = arredondar_2_casas(*saldo + valor_item);
+	
 	double troco = valor_pago - valor_item; 
-	return troco;
+	return arredondar_2_casas(troco);
 }
 
 int verificar_caixa(double saldo) {
-	if(saldo >= 1999.99){
+	if(saldo >= (1999.99 - 0.00001)){
 		return 1;
 	} else {
 		return 0;
@@ -25,11 +30,11 @@ int main(){
 	boas_vindas();
 	
 	for (i = 1; i <= 5000; i++){
-		troco += realizar_venda(&caixa, 0.20, 0.25);
+		troco = arredondar_2_casas(troco + realizar_venda(&caixa, 0.20, 0.25));
 	}
 	
-	printf("\n%.2lf", caixa);
-	printf("\n%.2lf", troco);
+	printf("\n%.50lf", caixa);
+	printf("\n%.50lf", troco);
 	
 	i = verificar_caixa(caixa);
 	if(i){
